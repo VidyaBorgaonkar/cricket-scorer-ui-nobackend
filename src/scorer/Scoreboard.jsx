437bd974081900cdb4c10d6ea75401d333a60c2b
himagirinathan <himagint@thoughtworks.com>
2018-06-19 12:18:08 +0530

    Story 1 : scoreboard implemented

--- src/scorer/Scoreboard.jsx
+++ src/scorer/Scoreboard.jsx
@@ -5,42 +5,56 @@ import {updateScore} from './scoreActions';
 import {initialScoreBoard} from './scoreReducer';
 
 const ScoreBoard = (props = initialScoreBoard) => {
-    return (<Container>
-        <br/>
-        <Row>
-            <Col md={{size: 6, offset: 3}}>
+    console.log({props})
+    const isOtherTeamPlayed = props.scoreboard[1].overs !== 0;
+    return (
+        <Container>
+            <br/>
+            <Row>
+                <Col md={{size: 6, offset: 3}}>
+                    <Row>
+                        <Col md="5" xs="4">
+                            <b>Team 1</b><br/>
+                        </Col>
+                        <Col sm="1" xs="2"/>
+                        <Col style={{textAlign: "right"}}>
+                            <b>{props.scoreboard[props.currentTeamIndex].total}/0 in 0/{props.totalNoOfOvers}</b>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            <br/>
+            {isOtherTeamPlayed ?
                 <Row>
-                    <Col md="5" xs="4">
-                        <b>Team 1</b><br/>
-                        <button onClick={() => props.updateScoreboard({run: 1})}>test</button>
+                    <Col md={{size: 6, offset: 3}} sm="12">
+                        <Row>
+                            <Col>
+                                Team 2 scored
+                            </Col>
+                            <Col style={{textAlign: "right"}}>
+                                120/5 in 12.1/{props.totalNoOfOvers}
+                            </Col>
+                        </Row>
                     </Col>
-                    <Col sm="1" xs="2"/>
-                    <Col style={{textAlign: "right"}}>
-                        <b>{props.scoreboard[props.currentTeamIndex].total}/5 in 12.1/20</b>
+                </Row> : <Row>
+                    <Col md={{size: 6, offset: 3}} sm="12">
+                        <Row>
+                            <Col>
+                                Team 2 yet to play
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
-            </Col>
-        </Row>
-        <br/>
-        <Row>
-            <Col md={{size: 6, offset: 3}} sm="12">
-                <Row>
-                    <Col>
-                        Team 2 scored
-                    </Col>
-                    <Col style={{textAlign: "right"}}>
-                        <b>120/5 in 12.1/20</b>
-                    </Col>
-                </Row>
-            </Col>
-        </Row>
-    </Container>)
+
+            }
+        </Container>);
 };
 
 const mapToProps = (state) => {
     return {
         currentTeamIndex: state.scoreInformation.currentTeamIndex,
-        scoreboard: state.scoreInformation.scoreboard
+        scoreboard: state.scoreInformation.scoreboard,
+        totalNoOfOvers: state.gameInformation.numberOfOvers
     }
 };
 
