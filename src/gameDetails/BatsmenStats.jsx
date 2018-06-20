388a8f79acfb22c574b7edd5f884a164c51aacde
himagirinathan <himagint@thoughtworks.com>
2018-06-20 22:31:00 +0530

    changed font size

---
+++ src/gameDetails/BatsmenStats.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Table, Container, Row, Col } from 'reactstrap';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import getBatsmenAggregateStats from './BatsmenAggregation';
+
+const addRow = (stats, index) => (
+  <tr key={index}>
+    <td>{stats.name}</td>
+    <td>{stats.runs}</td>
+    <td>{stats.totalBalls}</td>
+    <td>{stats.fours}</td>
+    <td>{stats.sixes}</td>
+    <td>{stats.strikeRate.toFixed(2)}</td>
+  </tr>
+);
+
+const BatsmenStats = props => (
+  <Container>
+    <Row>
+      <Col md={{ size: 6, offset: 3 }}>Batting Table</Col>
+      <Col md={{ size: 6, offset: 3 }}>
+        <Table striped bordered responsive style={{ fontSize: 'x-small' }}>
+          <thead>
+            <tr>
+              <th>Batsman</th>
+              <th>Runs</th>
+              <th>Balls</th>
+              <th>Fours</th>
+              <th>Sixes</th>
+              <th>Strike Rate</th>
+            </tr>
+          </thead>
+          <tbody>
+            {props.batsmen.map((stat, index) => addRow(stat, index))}
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
+  </Container>
+);
+
+BatsmenStats.propTypes = {
+  batsmen: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    runs: PropTypes.number.isRequired,
+    totalBalls: PropTypes.number.isRequired,
+    fours: PropTypes.number.isRequired,
+    sixes: PropTypes.number.isRequired,
+    strikeRate: PropTypes.arrayOf(PropTypes.number).isRequired,
+  })).isRequired,
+};
+
+const mapStateToProps = state => ({ batsmen: getBatsmenAggregateStats(state.balls) });
+
+export default connect(mapStateToProps)(BatsmenStats);
--- src/gameDetails/GameDetails.jsx
+++ src/gameDetails/GameDetails.jsx
@@ -3,11 +3,13 @@ import Container from 'reactstrap/lib/Container';
 import Row from 'reactstrap/lib/Row';
 import ScoreBoard from '../scorer/Scoreboard';
 import BowlingStats from './BowlingStats';
+import BatsmenStats from './BatsmenStats';
 
 const GameDetails = () => (
   <Container className="h-100">
     <Row className="h-100">
       <ScoreBoard />
+      <BatsmenStats />
       <BowlingStats />
     </Row>
 
