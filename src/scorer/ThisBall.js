be51a955a94691a3ed0035be5b3a516f52282b38
Prasanthi Sampathkumar <prasanthisampathkumar@INPrasanthi.local>
2018-06-20 11:47:09 +0530

    Breaking ThisBall into smaller components

--- src/scorer/ThisBall.js
+++ src/scorer/ThisBall.js
@@ -4,51 +4,61 @@ import {connect} from "react-redux";
 import {selectBatsman, selectRunsScored, updateScore} from "./scoreActions";
 
 
-const createRunsButton=(props,score)=>
-    <Button color="primary" className="mx-2 my-1 px-3 rounded" style={{flexBasis:40,flexShrink:0, flexGrow:0, width:40}} outline onClick={() => props.selectRunsScored(score)} active={props.selectedRuns === score}>{score}</Button>
+const createRunsButton = (props, score) =>
+    <Button color="primary" className="mx-2 my-1 px-3 rounded"
+            style={{flexBasis: 40, flexShrink: 0, flexGrow: 0, width: 40}} outline
+            onClick={() => props.selectRunsScored(score)} active={props.selectedRuns === score}>{score}</Button>
 
+
+const RunsToolBar = (props) =>
+    <ButtonGroup className="d-flex flex-wrap">
+        {createRunsButton(props, 0)}
+        {createRunsButton(props, 1)}
+        {createRunsButton(props, 2)}
+        {createRunsButton(props, 3)}
+        {createRunsButton(props, 4)}
+        {createRunsButton(props, 5)}
+        {createRunsButton(props, 6)}
+        {createRunsButton(props, 7)}
+    </ButtonGroup>;
+
+
+const BatsmenToolBar = (props) =>
+    <ButtonGroup>
+        <Button outline color="primary" onClick={() => props.selectBatsman(props.batsmen[0])}
+                active={props.selectedBatsman === props.batsmen[0]}>{props.batsmen[0]}</Button>
+        <Button outline color="primary" onClick={() => props.selectBatsman(props.batsmen[1])}
+                active={props.selectedBatsman === props.batsmen[1]}>{props.batsmen[1]}</Button>
+    </ButtonGroup>;
 
 const ThisBall = (props) =>
-            <Container>
-                <Row>
-                    <Col md={{size: 6, offset: 3}}>
-                        <b>This Ball</b>
-                    </Col>
-                </Row>
-                <br/>
-                <Row>
-                    <Col md={{size: 6, offset: 3}} className="text-center">
-                        <ButtonGroup>
-                            <Button outline color="primary" onClick={() => props.selectBatsman(props.batsmen[0])}
-                                    active={props.selectedBatsman === props.batsmen[0]}>{props.batsmen[0]}</Button>
-                            <Button outline color="primary" onClick={() => props.selectBatsman(props.batsmen[1])}
-                                    active={props.selectedBatsman === props.batsmen[1]}>{props.batsmen[1]}</Button>
-                        </ButtonGroup>
-                    </Col>
-                </Row>
-                <br/>
-                <Row>
-                    <Col md={{size: 6, offset: 3}} sm="12">
-                        <ButtonGroup className="d-flex flex-wrap">
-                            {createRunsButton(props,0)}
-                            {createRunsButton(props,1)}
-                            {createRunsButton(props,2)}
-                            {createRunsButton(props,3)}
-                            {createRunsButton(props,4)}
-                            {createRunsButton(props,5)}
-                            {createRunsButton(props,6)}
-                            {createRunsButton(props,7)}
-                        </ButtonGroup>
-                    </Col>
-                </Row>
-                <br/>
-                <Row>
-                    <Col md={{size: 6, offset: 3}}>
-                        <Button onClick={() => props.onNextBall(props)}>Next Ball</Button>
-                    </Col>
-                </Row>
-            </Container>;
-        
+    <Container>
+        <Row>
+            <Col md={{size: 6, offset: 3}}>
+                <b>This Ball</b>
+            </Col>
+        </Row>
+        <br/>
+        <Row>
+            <Col md={{size: 6, offset: 3}} className="text-center">
+                <BatsmenToolBar selectedBatsman={props.selectedBatsman} batsmen={props.batsmen}
+                                selectBatsman={props.selectBatsman}/>
+            </Col>
+        </Row>
+        <br/>
+        <Row>
+            <Col md={{size: 6, offset: 3}} sm="12">
+                <RunsToolBar selectedRuns={props.selectedRuns} selectRunsScored={props.selectRunsScored}/>
+            </Col>
+        </Row>
+        <br/>
+        <Row>
+            <Col md={{size: 6, offset: 3}}>
+                <Button onClick={() => props.onNextBall(props)}>Next Ball</Button>
+            </Col>
+        </Row>
+    </Container>;
+
 
 const mapStateToProps = (state) => {
     return {
