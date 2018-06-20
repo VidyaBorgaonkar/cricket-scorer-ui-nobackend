de5fdd50a5284754143fbd7d7b047a34b5c4e216
nehagupta2123 <nehagupta.dig@gmail.com>
2018-06-20 21:31:04 +0530

    Update overs played in the scoreboard

--- src/scorer/scoreReducer.js
+++ src/scorer/scoreReducer.js
@@ -2,7 +2,12 @@ import { NEXT_BALL } from '../store/actionConstants';
 
 export const initialScoreBoard = {
   currentTeamIndex: 0,
-  scoreboard: [{ total: 0, wickets: 0, overs: 0.0 }, { total: 0, wickets: 0, overs: 0.0 }],
+  scoreboard: [{
+    total: 0, wickets: 0, overs: 0.0, totalBalls: 0,
+  },
+  {
+    total: 0, wickets: 0, overs: 0.0, totalBalls: 0,
+  }],
 };
 
 const updateScoreboard = (state = initialScoreBoard, action) => {
@@ -10,25 +15,29 @@ const updateScoreboard = (state = initialScoreBoard, action) => {
     case NEXT_BALL: {
       const { extras } = action.payload;
       if (extras.length !== 0) {
-        for (let i = 0; i < action.payload.extras.length; i += 1) {
-          if (action.payload.extras.indexOf('W') !== -1 || action.payload.extras.indexOf('N') !== -1) {
+        for (let i = 0; i < extras.length; i += 1) {
+          if (extras.indexOf('W') !== -1 || extras.indexOf('N') !== -1) {
             return {
               currentTeamIndex: state.currentTeamIndex,
               scoreboard: [{
                 total: state.scoreboard[0].total + 1 + action.payload.runs,
                 wickets: 0,
-                overs: 0,
+                totalBalls: state.scoreboard[0].totalBalls,
+                overs: state.scoreboard[0].overs,
               }, state.scoreboard[1]],
             };
           }
         }
       }
+      const countBalls = state.scoreboard[0].totalBalls + 1;
       return {
         currentTeamIndex: state.currentTeamIndex,
         scoreboard: [{
           total: state.scoreboard[0].total + action.payload.runs,
           wickets: 0,
-          overs: 0,
+          totalBalls: countBalls,
+          overs: Math.floor(countBalls / 6) +
+          ((countBalls % 6) / 10),
         }, state.scoreboard[1]],
       };
     }
