3bff35007e058e058b302cd521720d285445f089
nithisht <nithisht@thoughtworks.com>
2018-06-20 16:19:35 +0530

    Fix failing test cases for overDetails

--- src/overDetails/reducer.test.js
+++ src/overDetails/reducer.test.js
@@ -32,17 +32,17 @@ describe('gameInformation/reducer', () => {
 
   it('should append the current ball extras to the current over', () => {
     const expectedState = { overDetails: ['B'], ballsRemaining: 5 };
-    expect(reducer(undefined, updateScore(undefined, 'B'))).toEqual(expectedState);
+    expect(reducer(undefined, updateScore(undefined, ['B']))).toEqual(expectedState);
   });
 
   it('should append the current ball runs & extras to the current over', () => {
     const expectedState = { overDetails: ['1B'], ballsRemaining: 5 };
-    expect(reducer(undefined, updateScore(1, 'B'))).toEqual(expectedState);
+    expect(reducer(undefined, updateScore(1, ['B']))).toEqual(expectedState);
   });
 
   it('should append the current ball runs & extras to the current over', () => {
     const expectedState = { overDetails: ['1N'], ballsRemaining: 6 };
-    expect(reducer(undefined, updateScore(1, 'N'))).toEqual(expectedState);
+    expect(reducer(undefined, updateScore(1, ['N']))).toEqual(expectedState);
   });
 
   it('should reset the state to initialState after an over', () => {
