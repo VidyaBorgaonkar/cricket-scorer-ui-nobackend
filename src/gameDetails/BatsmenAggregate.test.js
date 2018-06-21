79e81c2546c040e5cb4a9186cdd6388a219c6e9e
himagirinathan <himagint@thoughtworks.com>
2018-06-21 15:10:45 +0530

    changed font size

--- src/gameDetails/BatsmenAggregate.test.js
+++ src/gameDetails/BatsmenAggregate.test.js
@@ -8,8 +8,8 @@ describe('BatsmenAggregation/getBatsmenAggregateStats', () => {
     expect(getBatsmenAggregateStats(undefined)).toEqual([]);
   });
   it('should return batsmen stats for a single run', () => {
-    expect(getBatsmenAggregateStats([{ runs: 1, batsman: 'BatsmanName' }])).toEqual([{
-      name: 'BatsmanName',
+    expect(getBatsmenAggregateStats([{ runs: 1, batsman: 'BatsmanName', extras: [] }])).toEqual([{
+      name: 'BatsmanName*',
       runs: 1,
       totalBalls: 1,
       fours: 0,
@@ -18,8 +18,8 @@ describe('BatsmenAggregation/getBatsmenAggregateStats', () => {
     }]);
   });
   it('should return batsmen stats for a 4 runs', () => {
-    expect(getBatsmenAggregateStats([{ runs: 4, batsman: 'BatsmanName' }])).toEqual([{
-      name: 'BatsmanName',
+    expect(getBatsmenAggregateStats([{ runs: 4, batsman: 'BatsmanName', extras: [] }])).toEqual([{
+      name: 'BatsmanName*',
       runs: 4,
       totalBalls: 1,
       fours: 1,
@@ -28,8 +28,8 @@ describe('BatsmenAggregation/getBatsmenAggregateStats', () => {
     }]);
   });
   it('should return batsmen stats for a 6 runs', () => {
-    expect(getBatsmenAggregateStats([{ runs: 6, batsman: 'BatsmanName' }])).toEqual([{
-      name: 'BatsmanName',
+    expect(getBatsmenAggregateStats([{ runs: 6, batsman: 'BatsmanName', extras: [] }])).toEqual([{
+      name: 'BatsmanName*',
       runs: 6,
       totalBalls: 1,
       fours: 0,
@@ -37,15 +37,56 @@ describe('BatsmenAggregation/getBatsmenAggregateStats', () => {
       strikeRate: 600,
     }]);
   });
-
-  it('should return batsmen stats for a strike rate as ', () => {
-    expect(getBatsmenAggregateStats([{ runs: 1, batsman: 'BatsmanName' }, { runs: 0, batsman: 'BatsmanName' }, { runs: 1, batsman: 'BatsmanName' }])).toEqual([{
-      name: 'BatsmanName',
+  it('should return batsmen stats for a strike rate as 66.67', () => {
+    expect(getBatsmenAggregateStats([{ runs: 1, batsman: 'BatsmanName', extras: [] },
+      { runs: 0, batsman: 'BatsmanName', extras: [] },
+      { runs: 1, batsman: 'BatsmanName', extras: [] }])).toEqual([{
+      name: 'BatsmanName*',
       runs: 2,
       totalBalls: 3,
       fours: 0,
       sixes: 0,
       strikeRate: 66.67,
+    }]);
+  });
+  it('should return batsmen totalBalls as zero for a wide', () => {
+    expect(getBatsmenAggregateStats([{ runs: 0, extras: ['W'], batsman: 'BatsmanName' }])).toEqual([{
+      name: 'BatsmanName*',
+      runs: 0,
+      totalBalls: 0,
+      fours: 0,
+      sixes: 0,
+      strikeRate: 0,
+    }]);
+  });
+  it('should return batsmen totalBalls as zero for a No-Ball', () => {
+    expect(getBatsmenAggregateStats([{ runs: 0, extras: ['N'], batsman: 'BatsmanName' }])).toEqual([{
+      name: 'BatsmanName*',
+      runs: 0,
+      totalBalls: 0,
+      fours: 0,
+      sixes: 0,
+      strikeRate: 0,
+    }]);
+  });
+  it('should return batsmen totalBalls as one and runs as zero for a byes', () => {
+    expect(getBatsmenAggregateStats([{ runs: 1, extras: ['B'], batsman: 'BatsmanName' }])).toEqual([{
+      name: 'BatsmanName*',
+      runs: 0,
+      totalBalls: 1,
+      fours: 0,
+      sixes: 0,
+      strikeRate: 0,
+    }]);
+  });
+  it('should return batsmen totalBalls as one and runs as zero for a leg-byes', () => {
+    expect(getBatsmenAggregateStats([{ runs: 1, extras: ['LB'], batsman: 'BatsmanName' }])).toEqual([{
+      name: 'BatsmanName*',
+      runs: 0,
+      totalBalls: 1,
+      fours: 0,
+      sixes: 0,
+      strikeRate: 0,
     }]);
   });
 });
--- src/gameDetails/BatsmenAggregation.js
+++ src/gameDetails/BatsmenAggregation.js
@@ -1,39 +1,54 @@
-function initialBatsmanStats(batsman, runs) {
-  return {
-    name: batsman,
-    runs,
-    totalBalls: 1,
-    fours: runs === 4 ? 1 : 0,
-    sixes: runs === 6 ? 1 : 0,
-    strikeRate: runs * 100,
-  };
-}
+const getFoursCount = runs => (runs === 4 ? 1 : 0);
 
-function updateBatsmanStats(stats, batsman, runs) {
-  const totalBalls = stats[batsman].totalBalls + 1;
-  const totalNoOfRuns = stats[batsman].runs + runs;
-  const strikeRate = parseFloat(((totalNoOfRuns * 100) / totalBalls).toFixed(2));
+const getSixesCount = runs => (runs === 6 ? 1 : 0);
+
+const getStrikeRate = (totalNoOfRuns, totalBalls) =>
+  (totalBalls > 0 ? parseFloat(((totalNoOfRuns * 100) / totalBalls).toFixed(2)) : 0);
+
+const getBatsmenBallCount = extras => (extras.includes('W') || extras.includes('N') ? 0 : 1);
+
+const getBatsmenRuns = (extras, runs) => (extras.length === 0 ? runs : 0);
+
+const initialBatsmanStats = (batsman, runs, extras) => ({
+  name: `${batsman}*`,
+  runs: getBatsmenRuns(extras, runs),
+  totalBalls: getBatsmenBallCount(extras),
+  fours: getFoursCount(runs),
+  sixes: getSixesCount(runs),
+  strikeRate: getStrikeRate(getBatsmenRuns(extras, runs), getBatsmenBallCount(extras)),
+});
+
+const updateBatsmanStats = (stats, batsman, runs, wicket, extras) => {
+  const batsmenBallCount = getBatsmenBallCount(extras);
+  const playedBalls = stats[batsman].totalBalls;
+  const totalBalls = extras.length === 0 ? playedBalls + 1 : playedBalls + batsmenBallCount;
+  const totalNoOfRuns = stats[batsman].runs + getBatsmenRuns(extras, runs);
+  const name = wicket ? batsman : stats[batsman].name;
+  const strikeRate = getStrikeRate(totalNoOfRuns, totalBalls);
   return {
     ...stats[batsman],
+    name,
     runs: totalNoOfRuns,
     totalBalls,
-    fours: stats[batsman].fours + (runs === 4 ? 1 : 0),
-    sixes: stats[batsman].sixes + (runs === 6 ? 1 : 0),
+    fours: stats[batsman].fours + getFoursCount(runs),
+    sixes: stats[batsman].sixes + getSixesCount(runs),
     strikeRate,
   };
-}
+};
 
 const getBatsmenAggregateStats = (balls) => {
   if (balls === undefined) {
     return [];
   }
   const batsmenStats = balls.reduce((accumulator, ball) => {
-    const { batsman, runs } = ball;
+    const {
+      batsman, runs, wicket, extras,
+    } = ball;
     const stats = accumulator;
     if (batsman in stats) {
-      stats[batsman] = updateBatsmanStats(stats, batsman, runs);
+      stats[batsman] = updateBatsmanStats(stats, batsman, runs, wicket, extras);
     } else {
-      stats[batsman] = initialBatsmanStats(batsman, runs);
+      stats[batsman] = initialBatsmanStats(batsman, runs, extras);
     }
     return stats;
   }, {});
